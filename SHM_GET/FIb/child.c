0907ccec96f893437ab4cec564b73d1fd190cba0
NISCHAY HR <nischayhr11@gmail.com>
2024-08-27 19:28:09 +0530

    Implemented  Child process

---
+++ SHM_GET/FIb/child.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<sys/wait.h>
+#include<sys/shm.h>
+#include<sys/ipc.h>
+
+typedef struct node {
+
+int n;
+int a[];
+
+}shared;
+
+int main(){
+
+
+    int key=1234;
+    int shmid=shmget(key,sizeof(shared),0666|IPC_CREAT);
+
+    shared *ptr= (shared *)shmat(shmid,NULL,0666);
+
+    int n=ptr->n;
+
+    ptr->a[0]=0;
+    ptr->a[1]=1;
+
+    for(int i=2;i<=n;i++){
+
+        ptr->a[i]=ptr->a[i-1]+ptr->a[i-2];
+    }
+
+    shmdt((void *)ptr);
+}
